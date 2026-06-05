11d9e82f35ec369c889dfbc2c886cdf680cfbb74
Tanmay <86041392+tanmay-karde@users.noreply.github.com>
2026-06-05 18:58:37 +0530

    Update vector_store.py

--- backend/rag/vector_store.py
+++ backend/rag/vector_store.py
@@ -1,13 +1,9 @@
 import chromadb
-
 client = chromadb.PersistentClient(
     path="data/chroma_db"
 )
-
-collection = client.get_collection(
+collection = client.get_or_create_collection(
     "olympus_test"
 )
-
 results = collection.get()
-
 print(results)
